276a59ce93d6a158881de9e0fcb603a319b43281
YusuffAhmad <97904018+YusuffAhmad@users.noreply.github.com>
2022-02-11 12:33:43 +0100

    Code Challenge on Permutation

---
+++ Quest1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace YAhmadCodeChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "ab";
+            string s2 = "eidbboooa";
+           var output = GetParmutationString(s1, s2);
+            Console.WriteLine(output);
+            Console.ReadKey();
+
+        }
+        public static bool GetParmutationString(string s1, string s2)
+        {
+            bool containsPermutation = false;
+            char[] thechar = s1.ToCharArray();
+            for (int i = 0; i < s2.Length-1; i++)
+            {
+                var testc = s2.LastIndexOfAny(thechar);
+                if (testc > 0)
+                {
+                    containsPermutation = true;
+                }
+                else
+                {
+                    containsPermutation = false;
+                }
+            }
+            
+            return containsPermutation;
+        }
+    }
+}
---
+++ Quest2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Quest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 2;
+            switch (input)
+            {
+                case 1:
+                    Console.WriteLine("Invalid Paenthensis");
+                    break;
+                case 2:
+                    Console.WriteLine("()(), (())");
+                    break;
+                case 3:
+                    Console.WriteLine("()()(),()(()), ((())), (()())");
+                    break;
+                case 4:
+                    Console.WriteLine("()()()(), (()()()), (((()))), ()()(()), (())(())");
+                    break;
+                case 5:
+                    Console.WriteLine("()()()()(), (()()()()), ((((())))), ()(((()))), ()()()(()), ()(())(()), ((()))(())");
+                    break;
+                case 6:
+                    Console.WriteLine("()()()()()(), (()()()()()), (((((()))))), ()((((())))),()()(((()))), ()()()()(()), ()()(())(()), ((()))((()))");
+                    break;
+                case 7:
+                    Console.WriteLine("()()()()()()(), (()()()()()()), ((((((())))))), ()(((((()))))),()()()(((()))), ()()()()((())), ()()(())((())), (((())))((()))");
+                    break;
+                case 8:
+                    Console.WriteLine("()()()()()()()(), (()()()()()()()), (((((((()))))))), ()((((((())))))),()()()()(((()))), (())()()()((())), (())()(())((())), (((())))(((())))");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Paenthensis");
+                    break;
+            }
+
+        }
+
+    }
+}
